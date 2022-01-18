3e33e7c00fe641d8c6d18cf9fd8bd99c9fcd269d
Adrian Kiva <akiva@mta.ca>
2022-01-18 18:24:15 -0500

    format spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,15 +7,27 @@ export default function DayListItem(props) {
 
   const {name, spots, setDay } = props;
 
-  const dayClass = classNames("day-list_item", {
+  const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": spots===0
   });
 
+  const formatSpots = (spots) => {
+    if (spots === 0) {
+      return "no spots remaining"
+    }
+    else if (spots === 1) {
+      return "1 spot remaining"
+    }
+    else  {
+      return `${spots} spots remaining`
+    }
+  }
+
   return (
     <li onClick={() => setDay(name)} className={dayClass}>
       <h2 className="text--regular">{name}</h2> 
-      <h3 onClick="text--light">{spots} spots remaining</h3>
+      <h3 onClick="text--light">{formatSpots(spots)}</h3>
     </li>
   );
 }
