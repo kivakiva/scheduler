8db06772f609db754bb7f26061be1883ac38e17b
Adrian Kiva <akiva@mta.ca>
2022-01-19 18:25:19 -0500

    add dayListItem functionality

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -4,13 +4,18 @@ import InterviewerListItem from "components/InterviewerListItem";
 export default function InterviewerList(props) {
 
   const mockInterviewers = [
-    {name: "Abraham"},
-    {name: "johnny"},
-    {name: "bella"}
+    {name: "Abraham",
+  id:1},
+    {name: "johnny",
+  id:2},
+    {name: "bella",
+  id:3}
   
   ]
 
-  const interviewers = mockInterviewers.map((interviewer) => <InterviewerListItem {...interviewer} />)
+  const [interviewer, setInterviewer] = useState("no interviewer")
+
+  const interviewers = mockInterviewers.map((interviewerData) => <InterviewerListItem {...interviewerData} key={interviewer.id} interviewer={interviewer} setInterviewer={setInterviewer}/>)
 
   return (
     <ul>
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -1,11 +1,27 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
 
 export default function InterviewerListItem(props) {
 
   const { name, id, avatar, selected, setInterviewer } = props;
 
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": selected
+  });
+
+  const interviewerImageClass = `${interviewerClass} ${interviewerClass}-image`;
+
   return (
-    <h3>{name}</h3>
+    <li className={interviewerClass} onClick={() => setInterviewer(id)}>
+  <img
+    className={interviewerImageClass}
+    src={avatar}
+    alt={name}
+  />
+  {name}
+</li>
+
   )
 }
 
