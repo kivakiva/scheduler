34d8701fbc7c0db79dc607315e099ecd32d59f1a
Adrian Kiva <akiva@mta.ca>
2022-01-22 14:53:56 -0500

    begin use visual mode for appt

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,14 +4,28 @@ import Header from "components/Appointment/Header.js";
 import Show from "components/Appointment/Show.js";
 import Empty from "components/Appointment/Empty.js";
 
+import useVisualMode from "hooks/useVisualMode.js";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
 export default function Appointment(props) {
 
   const { interview, time } = props;
 
+  const { mode, transition, back }  = useVisualMode( 
+    interview ? SHOW : EMPTY );
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer.name} /> : <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show 
+          student={interview.student} 
+          interviewer={interview.interviewer.name} 
+        />
+      )}
     </article>
   )
 };
